4ee15a097de52adc74595043ca0c4357060d2c6e
Graeme Gellatly <gdgellatly@gmail.com>
2020-07-28 14:50:10 +1200

    Lead Influencer Enhancements

--- crm_lead_influencer/models/crm_lead_influencer.py
+++ crm_lead_influencer/models/crm_lead_influencer.py
@@ -8,20 +8,24 @@ class CrmLeadInfluencer(models.Model):
 
     _name = "crm.lead.influencer"
     _description = "Influencer"
+    _order = "sequence, id"
 
     lead_id = fields.Many2one("crm.lead", string="Lead")
     role_id = fields.Many2one("crm.lead.influencer.role", string="Role")
     partner_id = fields.Many2one("res.partner", string="Partner")
     phone = fields.Char("Phone")
     mobile = fields.Char("Mobile")
-    name = fields.Char()
+    name = fields.Char(string="Company Name")
+    contact_name = fields.Char()
     email = fields.Char()
     note = fields.Text()
+    sequence = fields.Integer(default=10)
 
     @api.onchange("partner_id")
     def onchange_partner(self):
         if self.partner_id:
             self.phone = self.partner_id.phone
             self.mobile = self.partner_id.mobile
-            self.name = self.partner_id.name
+            self.contact_name = self.partner_id.name
+            self.name = self.partner_id.parent_id.name
             self.email = self.partner_id.email
